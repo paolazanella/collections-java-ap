2182aae54e8c5ed1c006959a14c094134db093dc
Paola Zanella <pzanella@edu.univali.br>
2024-06-14 15:57:57 -0300

    exercicio princiapal

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    //atribudo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
