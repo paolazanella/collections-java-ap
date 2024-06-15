c1dc14f9be1bc577efb00c1553d3549ea1faf942
Paola Zanella <pzanella@edu.univali.br>
2024-06-15 17:46:11 -0300

    Exercisio de criar lista em operacoes basicas

--- src/main/java/list/OperacoesBasicas/Tarefa.java
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -1,7 +1,7 @@
 package main.java.list.OperacoesBasicas;
 
 public class Tarefa {
-    //atribudo
+    //atributo
     private String descricao;
 
     public Tarefa(String descricao) {
@@ -10,5 +10,10 @@ public class Tarefa {
 
     public String getDescricao() {
         return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return  descricao;
     }
 }
